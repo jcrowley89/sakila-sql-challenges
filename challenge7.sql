bd4bbbddff24d99cc997ac1ba8af892a3e0c5101
John Crowley <johncrowleydev@gmail.com>
2021-08-25 19:11:53 -0400

    finally solved challenge7 :)

--- challenge7.sql
+++ challenge7.sql
@@ -6,7 +6,27 @@ Calculate late fees on the following schedule for late rental returns:
 	- 6 or more days late is $12
 */
 
-SELECT * FROM rental 
-WHERE return_date IS NULL
-
---instructions unclear, saving this challenge for later
+SELECT 	first_name, last_name,
+	CONCAT( '$', SUM(
+		CASE
+			WHEN days_late = 1 THEN 150
+			WHEN days_late = 2 THEN 300
+			WHEN days_late BETWEEN 3 AND 5 THEN 850
+			ELSE 1200 
+		END)/100) AS late_fees
+FROM (
+	SELECT p.payment_id, c.first_name, c.last_name, r.rental_date, 
+		r.return_date, f.rental_duration,
+		DATEDIFF(day, r.rental_date, r.return_date) - f.rental_duration AS days_late
+	FROM payment p
+	LEFT JOIN customer c
+	ON p.customer_id = c.customer_id
+	LEFT JOIN rental r
+	ON p.rental_id = r.rental_id
+	LEFT JOIN inventory i
+	ON r.inventory_id = i.inventory_id
+	LEFT JOIN film f
+	ON i.film_id = f.film_id
+	WHERE DATEDIFF(day, r.rental_date, r.return_date) > f.rental_duration
+	) as dt
+GROUP BY first_name, last_name
