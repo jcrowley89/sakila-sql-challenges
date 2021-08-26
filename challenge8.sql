403bc5054acfa395384f361ead2510d55a9c185d
John Crowley <johncrowleydev@gmail.com>
2021-08-26 08:23:53 -0400

    challenge8

--- challenge8.sql
+++ challenge8.sql
@@ -1,19 +1,11 @@
 /*
-CHALLENGE 7
-Calculate late fees on the following schedule for late rental returns: 
-	- the first and second late day is charged at $1.50 per day
-	- 3-5 days late is a flat $8.50 fee
-	- 6 or more days late is $12
+CHALLENGE 8
+Find customers who have returned rentals late 10 times or more, so that
+you can send them a mailer about your new loyalty program that will charge
+no late fees as long as they're part of the program
 */
 
-SELECT 	first_name, last_name,
-	CONCAT( '$', SUM(
-		CASE
-			WHEN days_late = 1 THEN 150
-			WHEN days_late = 2 THEN 300
-			WHEN days_late BETWEEN 3 AND 5 THEN 850
-			ELSE 1200 
-		END)/100.0) AS late_fees
+SELECT 	first_name, last_name, COUNT(*) AS late_returns
 FROM (
 	SELECT p.payment_id, c.first_name, c.last_name, r.rental_date, 
 		r.return_date, f.rental_duration,
@@ -30,3 +22,4 @@ FROM (
 	WHERE DATEDIFF(day, r.rental_date, r.return_date) > f.rental_duration
 	) as dt
 GROUP BY first_name, last_name
+HAVING COUNT(*) >= 10
